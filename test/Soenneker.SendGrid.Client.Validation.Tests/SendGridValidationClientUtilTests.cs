a58ad09c5c8bb090d7da103cea66e73b8f66cf95
Jake Soenneker <jake@soeneker.com>
2024-12-18 19:35:00 +0000

    xUnit v3

--- test/Soenneker.SendGrid.Client.Validation.Tests/SendGridValidationClientUtilTests.cs
+++ test/Soenneker.SendGrid.Client.Validation.Tests/SendGridValidationClientUtilTests.cs
@@ -1,7 +1,7 @@
 using Soenneker.SendGrid.Client.Validation.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
-using Xunit.Abstractions;
+
 
 namespace Soenneker.SendGrid.Client.Validation.Tests;
 
