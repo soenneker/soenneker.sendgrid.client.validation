f9a9357d27d6dbb7a230b0b75aee08bf9e17cef9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:42 -0500

    xml commenting

--- src/Soenneker.SendGrid.Client.Validation/SendGridValidationClientUtil.cs
+++ src/Soenneker.SendGrid.Client.Validation/SendGridValidationClientUtil.cs
@@ -43,11 +43,18 @@ public sealed class SendGridValidationClientUtil : ISendGridValidationClientUtil
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(_clientId);
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(_clientId);
